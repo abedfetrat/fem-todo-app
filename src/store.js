a464d1ec121a9b2365abaf6adbe237ba8bc1d76c
Abed Fetrat <abedfetrat@gmail.com>
2022-12-14 21:45:15 +0100

    persist redux state to localstorage

--- src/store.js
+++ src/store.js
@@ -2,9 +2,22 @@ import { configureStore } from "@reduxjs/toolkit";
 import filtersReducer from "./filtersSlice";
 import todosReducer from "./todosSlice";
 
+const loadStateFromStorage = () => {
+    const persistedState = JSON.parse(localStorage.getItem('app_state'));
+    return persistedState ? persistedState : {};
+};
+
+const saveStateToStorage = () => {
+    const state = store.getState();
+    localStorage.setItem('app_state', JSON.stringify(state));
+};
+
 export const store = configureStore({
     reducer: {
         todos: todosReducer,
         filters: filtersReducer
-    }
+    },
+    preloadedState: loadStateFromStorage()
 });
+
+store.subscribe(saveStateToStorage);
